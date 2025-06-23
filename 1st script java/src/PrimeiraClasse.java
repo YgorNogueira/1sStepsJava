8be09101eef3384a16cbcba087d6c9ed0fcc7bd3
Ygor Nogueira <ygornogueiramacedo@gmail.com>
2025-06-22 22:23:58 -0300

    Add files via upload

---
+++ 1st script java/src/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.print("Concluí a aula 1 e estou mergulhando em java!");
+    }
+}
