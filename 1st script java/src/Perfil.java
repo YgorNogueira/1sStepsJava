c97addf5111971d60f8c9d765ea354bcf370634f
YgorNogueira <ygornogueiramacedo@gmail.com>
2025-06-22 23:05:41 -0300

    feat/ subtracao perfil e soma

---
+++ 1st script java/src/Perfil.java
@@ -0,0 +1,12 @@
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.print("Olá, Ygor!");
+        tudoBem();
+    }
+
+    public static void tudoBem()
+    {
+        System.out.print("Tudo bem!");
+    }
+
+}
